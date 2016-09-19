9574655735970688e9989f7a4670505cdab31c99
Jozef Izso <jozef.izso@gmail.com>
2016-09-19 18:53:00 +0200

    Sample build script with TeamCity messages

--- build.cmd
+++ build.cmd
@@ -1,1 +1,13 @@
-echo Building using TeamCity server.
+@echo off
+
+echo ##teamcity[blockOpened name='build' description='Compile project']
+echo Building project using build script.
+echo   Project1 -^> Project1.dll
+echo Build complete.
+echo ##teamcity[blockClosed name='build']
+
+echo ##teamcity[blockOpened name='nunit3' description='Running tests']
+echo nunit3-console
+echo Testing Project1.Tests.dll
+echo Testing complete.
+echo ##teamcity[blockClosed name='nunit3']
