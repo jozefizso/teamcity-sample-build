b9a049937b173ae851bc38eb548fcecaec28fd99
Jozef Izso <jozef.izso@gmail.com>
2016-09-19 19:02:00 +0200

    Add TeamCity messages for unit tests

--- build.cmd
+++ build.cmd
@@ -9,5 +9,17 @@ echo ##teamcity[blockClosed name='build']
 echo ##teamcity[blockOpened name='nunit3' description='Running tests']
 echo nunit3-console
 echo Testing Project1.Tests.dll
+
+echo ##teamcity[testSuiteStarted name='Project1.Tests.dll' flowId='0-1124']
+echo ##teamcity[flowStarted flowId='0-1002' parent='0-1124']
+echo ##teamcity[testStarted name='Project1.isAuthenticated_AnyLogonUserResult_AlwaysClosesHandle(True)' captureStandardOutput='false' flowId='0-1002']
+echo ##teamcity[testFinished name='Project1.isAuthenticated_AnyLogonUserResult_AlwaysClosesHandle(True)' duration='356' flowId='0-1002']
+echo ##teamcity[flowFinished flowId='0-1002']
+echo ##teamcity[flowStarted flowId='0-1003' parent='0-1124']
+echo ##teamcity[testStarted name='Project1.isAuthenticated_AnyLogonUserResult_AlwaysClosesHandle(False)' captureStandardOutput='false' flowId='0-1003']
+echo ##teamcity[testFinished name='Project1.isAuthenticated_AnyLogonUserResult_AlwaysClosesHandle(False)' duration='16' flowId='0-1003']
+echo ##teamcity[flowFinished flowId='0-1003']
+echo ##teamcity[testSuiteFinished name='Project1.Tests.dll' flowId='0-1124']
+
 echo Testing complete.
 echo ##teamcity[blockClosed name='nunit3']
